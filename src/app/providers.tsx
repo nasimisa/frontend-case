9a5304b8c118c5a74537f20e4fe8b52b5c9444dd
nasimisa <nasimi.sadiqzade@gmail.com>
2025-12-25 17:25:24 +0400

    chore: Initial project set up

---
+++ src/app/providers.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactNode, useState } from 'react';
+import theme from '@/shared/theme';
+
+export default function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        {children}
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
+}
